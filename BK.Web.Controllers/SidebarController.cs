ba58cd5cdaebbabb041386c5042adeec3e19ee8c
HZL <HZL420zq@hotmail.com>
2017-10-11 17:40:57 +0800

    clock

---
+++ BK.Web.Controllers/SidebarController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace BK.Web.Controllers
+{
+    public class SidebarController : Controller
+    {
+        public PartialViewResult Index(string type)
+        {
+            return PartialView();
+        }
+    }
+}
--- BK.Web/res/js/app.js
+++ BK.Web/res/js/app.js
@@ -1,10 +1,9 @@
 ﻿
-layui.define(['layer', 'laypage', 'laytpl', 'element', 'form', 'layedit', 'util', 'flow', 'code'], function (exports) {
+layui.define(['layer', 'laypage', 'laytpl', 'element','util', 'flow'], function (exports) {
     var $ = layui.jquery
         , laypage = layui.laypage
         , laytpl = layui.laytpl
         , layer = layui.layer
-        , form = layui.form
         , flow = layui.flow
         , util = layui.util
         , device = layui.device();
@@ -14,24 +13,123 @@ layui.define(['layer', 'laypage', 'laytpl', 'element', 'form', 'layedit', 'util'
     }
     //固定Bar
     util.fixbar();
-    var latestPostsTpl;
-    $.get("/TempLate/latestPosts", function (tpl) {
-        latestPostsTpl = tpl;
-        if ($("#latestPosts").length > 0) {
-            flow.load({
-                elem: '#latestPosts',
-                done: function (page, next) {
-                    var lis = [];
-                    $.get("/Posts/lastest", { index: page, count:5 }, function (data) {
-                        var result = laytpl(latestPostsTpl).render(data.list);
-                        lis.push(result);
-                        next(lis.join(''), page < data.pages); 
-                    });
-                }
-            })
-        }
-    })
 
+    //时钟
+    var clock = $('#clock'),
+		alarm = clock.find('.alarm'),
+		ampm = clock.find('.ampm');
+    var alarm_counter = -1;
+
+    var digit_to_name = 'zero one two three four five six seven eight nine'.split(' ');
+
+    var digits = {};
+    var positions = [
+		'h1', 'h2', ':', 'm1', 'm2', ':', 's1', 's2'
+    ];
+    var digit_holder = clock.find('.digits');
+    $.each(positions, function () {
+
+        if (this == ':') {
+            digit_holder.append('<div class="dots">');
+        }
+        else {
+
+            var pos = $('<div>');
+
+            for (var i = 1; i < 8; i++) {
+                pos.append('<span class="d' + i + '">');
+            }
+
+            // Set the digits as key:value pairs in the digits object
+            digits[this] = pos;
+
+            // Add the digit elements to the page
+            digit_holder.append(pos);
+        }
+
+    });
+
+    // Add the weekday names
+
+    var weekday_names = 'MON TUE WED THU FRI SAT SUN'.split(' '),
+		weekday_holder = clock.find('.weekdays');
+
+    $.each(weekday_names, function () {
+        weekday_holder.append('<span>' + this + '</span>');
+    });
+
+    var weekdays = clock.find('.weekdays span');
+
+    (function update_time() {
+
+        // Use moment.js to output the current time as a string
+        // hh is for the hours in 12-hour format,
+        // mm - minutes, ss-seconds (all with leading zeroes),
+        // d is for day of week and A is for AM/PM
+
+        var now = moment().format("hhmmssdA");
+
+        digits.h1.attr('class', digit_to_name[now[0]]);
+        digits.h2.attr('class', digit_to_name[now[1]]);
+        digits.m1.attr('class', digit_to_name[now[2]]);
+        digits.m2.attr('class', digit_to_name[now[3]]);
+        digits.s1.attr('class', digit_to_name[now[4]]);
+        digits.s2.attr('class', digit_to_name[now[5]]);
+
+        // The library returns Sunday as the first day of the week.
+        // Stupid, I know. Lets shift all the days one position down, 
+        // and make Sunday last
+
+        var dow = now[6];
+        dow--;
+
+        // Sunday!
+        if (dow < 0) {
+            // Make it last
+            dow = 6;
+        }
+
+        // Mark the active day of the week
+        weekdays.removeClass('active').eq(dow).addClass('active');
+
+        // Set the am/pm text:
+        ampm.text(now[7] + now[8]);
+
+
+        // Is there an alarm set?
+
+        if (alarm_counter > 0) {
+
+            // Decrement the counter with one second
+            alarm_counter--;
+
+            // Activate the alarm icon
+            alarm.addClass('active');
+        }
+        else if (alarm_counter == 0) {
+
+            time_is_up.fadeIn();
+
+            // Play the alarm sound. This will fail
+            // in browsers which don't support HTML5 audio
+
+            try {
+                $('#alarm-ring')[0].play();
+            }
+            catch (e) { }
+
+            alarm_counter--;
+            alarm.removeClass('active');
+        }
+        else {
+            // The alarm has been cleared
+            alarm.removeClass('active');
+        }
+
+        // Schedule this function to be run again in 1 sec
+        setTimeout(update_time, 1000);
+
+    })();
 
     exports('app', {});
 }); 
